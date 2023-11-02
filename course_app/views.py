6a4f04a1f9692fe2741dd19aee7239f8e91ac430
umeshregmi <regmiumesh51@gmail.com>
2023-11-02 17:03:22 +0545

    new commit add new code

--- course_app/views.py
+++ course_app/views.py
@@ -4,5 +4,4 @@ from course_app.models import Student
 
 # Create your views here.
 def home(request):
-    student = Student.objects.all()[0]
-    return render(request, 'course_app/home.html', {'student': student})
+    return render(request, 'course_app/home.html')
